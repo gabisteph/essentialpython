61e509cc56fb6c55293e399f3b37778112afb8b9
gabisteph <gabriellepiresmestrinho@gmail.com>
2023-04-25 16:07:29 -0400

    tipo booleano

---
+++ tipo_booleano.py
@@ -0,0 +1,16 @@
+"""
+Tipo booleano
+
+Algebra Booleana, criada por George Boole
+
+2  constantes, Verdadeiro ou Falso
+
+True -> verdadeiro
+
+False -> Falso
+
+OBS: Sempre com a inicial maiúscula
+not inverte o resultado
+
+
+"""
