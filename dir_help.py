820fce0b128e3103312becb295aa373e393666e6
gabisteph <gabriellepiresmestrinho@gmail.com>
2023-04-25 14:57:37 -0400

    dir e help

---
+++ dir_help.py
@@ -0,0 +1,9 @@
+"""
+utilitários Python para auxiliar na programação
+
+dir -> Apresenta todos os atributos e funções/métodos disponíveis para determinado tipo de dado ou variável
+dir(tipo de dado)
+Help -> Apresenta documentação/como utilizar os atributos/propriedades/métodos disponíveis para determinado tiṕo de dado ou variável
+help(tipodedado.função)
+
+"""
