88382eb63b18514fd2bb2c330915a2dd025b2107
gabisteph <gabriellepiresmestrinho@gmail.com>
2023-04-25 16:51:46 -0400

    tipo escopo de variavel

---
+++ escopo_de_variavel.py
@@ -0,0 +1,20 @@
+"""
+Escopo de variáveis
+
+Escopo é a limitação de algo
+
+Dois casos de escopo:
+
+1- Variáveis globais;
+        - são reconhecidas por todo o programa.
+2 - Variáveis Locais;
+        - são reconhecidas apenas no bloco onde foram declaradas
+Para declarar variáveis em python fazemos:
+
+nome_da_variavel = valor_da_variavel
+
+python é uma linguagem de tipagem dinâmica. Isso significa que ao declararmos uma variável, nós não colocamos o tipo de dado dela. Este tipo é inferido ao atribuímos o valor à mesma.
+
+
+"""
+
