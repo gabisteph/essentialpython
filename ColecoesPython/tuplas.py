f176dc9745237be59fa668122ccaeaf92ff2797b
gabisteph <gabriellepiresmestrinho@gmail.com>
2023-05-29 13:31:38 -0400

    tuplas

---
+++ ColecoesPython/tuplas.py
@@ -0,0 +1,11 @@
+"""
+Tuplas (Tuple)
+
+representadas por parenteses ou não
+tupla1 = (1, 2, 3, 4, 5)
+tupla2 = 1, 2, 3, 4, 5
+são imutáveis
+tupla de 1 elemento são assim:
+tupla4 = (4,)
+ pode-se concluir que as tuplas são definidas pela vírgula e não pelo parênteses
+"""
